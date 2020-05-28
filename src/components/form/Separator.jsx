1bedbf4de5ecb9897f8140a35cada3ad6951c328
Cristopher Alvear Candia <c_alvear@live.cl>
2020-05-28 11:30:33 -0400

    feat: formfactory

--- src/components/form/Separator.jsx
+++ src/components/form/Separator.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Divider } from 'semantic-ui-react';
 
-function Separator({ label, divider })
+export default function Separator({ label, divider })
 {
     return (
         <div className='grid-form-separator'>
@@ -10,5 +10,3 @@ function Separator({ label, divider })
         </div>
     );
 }
-
-export default Separator;
