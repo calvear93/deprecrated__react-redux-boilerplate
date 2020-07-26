12e60d02f5e2bf9d10881696f2f11dcc19951004
Cristopher Alvear Candia <c_alvear@live.cl>
2020-07-26 14:27:59 -0400

    feat: sw added

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ ReactDOM.render(
     <App />,
     document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+if (process.env.REACT_APP_SERVICE_WORKER === 'true')
+{
+    const serviceWorker = require('./serviceWorker');
+    serviceWorker.register();
+}
