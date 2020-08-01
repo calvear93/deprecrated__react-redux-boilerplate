34a2939661f854c72371aaa189b21702715687b0
Cristopher Alvear Candia <c_alvear@live.cl>
2020-08-01 19:58:55 -0400

    refactor: sourcemaps generation tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,9 @@
 const { when } = require('@craco/craco');
-const sassSourcemaps = require('./webpack/sass-sourcemaps');
 
 module.exports = {
     plugins: [
-        // ...when(process.env.NODE_ENV === 'development', () => [
-        //     sassSourcemaps
-        // ], [])
+        ...when(process.env.GENERATE_SOURCEMAP === 'true' && process.env.NODE_ENV === 'development', () => [
+            require('./webpack/sass-sourcemaps')
+        ], [])
     ]
 };
