6d499c4061fde040985cbb80b00a9d654ad925d2
Cristopher Alvear Candia <c_alvear@live.cl>
2020-08-02 12:29:29 -0400

    chore: twks

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const { when } = require('@craco/craco');
 
 module.exports = {
+    devServer: {
+        compress: false,
+        port: 3000,
+        headers: {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'X-XSS-Protection': '1; mode=block'
+        }
+    },
     plugins: [
         ...when(process.env.GENERATE_SOURCEMAP === 'true' && process.env.NODE_ENV === 'development', () => [
             require('./webpack/sass-sourcemaps')
