ab46397baadfb77b1f671242f4bbd3d3803ed6cd
Cristopher Alvear <c_alvear@live.cl>
2020-08-29 10:58:29 -0400

    chore: libraries updated

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,28 @@ module.exports = {
         }
     },
     plugins: [
+        {
+            plugin: require('craco-image-optimizer-plugin'),
+            options: {
+                mozjpeg: {
+                    progressive: true,
+                    quality: 65
+                },
+                optipng: {
+                    enabled: false
+                },
+                pngquant: {
+                    quality: [ 0.65, 0.9 ],
+                    speed: 4
+                },
+                gifsicle: {
+                    interlaced: false
+                },
+                webp: {
+                    quality: 75
+                }
+            }
+        },
         ...when(process.env.GENERATE_SOURCEMAP === 'true' && process.env.NODE_ENV === 'development', () => [
             require('./webpack/sass-sourcemaps')
         ], [])
